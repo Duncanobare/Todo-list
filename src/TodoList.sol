299b5e6eb246a5b5e027274456dd0c2769139d5f
Duncan Obare <95039318+Duncanobare@users.noreply.github.com>
2024-07-29 11:32:14 -0300

    Update TodoList.sol
    Explicitly setting endAt
    corrected  return type  of viewTask function

--- src/TodoList.sol
+++ src/TodoList.sol
@@ -8,7 +8,8 @@ contract TodoList {
         string taskName;
         bool completed;
         uint256 startAt;
-        uint256 endAt;
+        uint256 duration;
+        uint256 endAt; 
     }
 
     Task[] private tasks;
@@ -23,45 +24,44 @@ contract TodoList {
     }
 
     function addTask(
-        string memory taskName,
-        uint256 startAt,
-        uint256 endAt,
-        bool completed
+        string memory _taskName,
+        uint256 _startAt,
+        uint256 _duration
     ) public onlyOwner {
-        tasks.push(Task(taskName, completed, startAt, endAt));
+        Task memory ts = Task({
+            taskName: _taskName,
+            startAt: _startAt,
+            duration: _duration,
+            endAt: _startAt + _duration, // Explicitly setting endAt
+            completed: false
+        });
+        tasks.push(ts);
     }
 
     function viewTasksCount() public view onlyOwner returns (uint256) {
         return tasks.length;
     }
 
-    function viewTask(
-        uint256 index
-    ) public view onlyOwner returns (string memory, bool, uint256, uint256) {
+    function viewTask(uint256 index) public view onlyOwner returns (Task memory) { // Corrected return type
         require(index < tasks.length, "task absent");
-        Task memory task = tasks[index];
-        return (task.taskName, task.completed, task.startAt, task.endAt);
+        return tasks[index]; 
     }
 
     function timeTaken(uint256 index) public view onlyOwner returns (uint256) {
         require(index < tasks.length, "task absent");
-        Task memory task = tasks[index];
-        require(
-            task.endAt >= task.startAt,
-            "end time must be after start time"
-        );
+        Task storage task = tasks[index]; 
+        require(task.endAt >= task.startAt, "end time must be after start time");
         return task.endAt - task.startAt;
     }
 
     function taskCompleted(uint256 index) public view onlyOwner returns (bool) {
         require(index < tasks.length, "task absent");
-        Task memory task = tasks[index];
+        Task storage task = tasks[index];
         return task.completed;
     }
 
     function markTaskComplete(uint256 index) public onlyOwner {
         require(index < tasks.length, "task absent");
-        tasks[index].completed = true;
+        tasks[index].completed = true; 
     }
 }
-//127.0.0.1:8545
